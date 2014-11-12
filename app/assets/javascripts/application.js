6c2b10a33c676a7dd71ad49ddfbf152e51fbd32f
Camilla van Klinken <camillavk@gmail.com>
2014-11-12 11:53:37 +0000

    reattempting modals with bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,11 +23,71 @@
 //   $(this).modal();
 // });
 
-$(function(){
-  $('a').click(function(event) {
+// $(function(){
+//   $('a').click(function(event) {
+//     event.preventDefault();
+//     $.get(this.href, function(html) {
+//       $(html).appendTo('body').modal();
+//     });
+//   });
+// });
+
+$(document).ready(function(){
+
+  $('a').click(function(event){
     event.preventDefault();
-    $.get(this.href, function(html) {
-      $(html).appendTo('body').modal();
-    });
+    $(this).modal();
   });
-});
+
+//   $(document).bind('ajaxError', 'form#new_user', function(event, jqxhr, settings, exception){
+//
+//     // note: jqxhr.responseJSON undefined, parsing responseText instead
+//     $(event.data).render_form_errors( $.parseJSON(jqxhr.responseText) );
+//
+//   });
+//
+//   $('#new_user_modal').on('click', function() {
+//     var options = {
+//       show: true
+//     }
+//     $('#modal-').modal(options);
+//   })
+// });
+//
+// (function($) {
+//
+//   $.fn.modal_success = function(){
+//     // close modal
+//     this.modal('hide');
+//
+//     // clear form input elements
+//     // todo/note: handle textarea, select, etc
+//     this.find('form input[type="text"]').val('');
+//
+//     // clear error state
+//     this.clear_previous_errors();
+//   };
+//
+//   $.fn.render_form_errors = function(errors){
+//
+//     $form = this;
+//     this.clear_previous_errors();
+//     model = this.data('model');
+//
+//     // show error messages in input form-group help-block
+//     $.each(errors, function(field, messages){
+//       $input = $('input[name="' + model + '[' + field + ']"]');
+//       $input.closest('.form-group').addClass('has-error').find('.help-block').html( messages.join(' & ') );
+//     });
+//
+//   };
+//
+//   $.fn.clear_previous_errors = function(){
+//     $('.form-group.has-error', this).each(function(){
+//       $('.help-block', $(this)).html('');
+//       $(this).removeClass('has-error');
+    // });
+  // }
+
+
+}(jQuery));
