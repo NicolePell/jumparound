306a515f62760776218a7ad2052b2ce265144bb4
Camilla van Klinken <camillavk@gmail.com>
2014-11-14 12:48:24 +0000

    Gallery and Shop tests working

--- spec/features/likes_features_spec.rb
+++ spec/features/likes_features_spec.rb
@@ -3,31 +3,44 @@ require 'rails_helper'
 describe 'Liking pictures' do
 
   before do
-    Sighting.create(caption: 'In the toon')
+    Sighting.create(caption: 'In the toon', seen_at: '12/03/2014')
   end
 
   context 'when signed in' do
     before do
       visit '/'
-      click_link 'Sign up'
-      fill_in 'Email', with: 'test@test.com'
-      fill_in 'Password', with: 'testtest'
-      fill_in 'Password confirmation', with: 'testtest'
-      click_button 'Sign up'
+      first(:link, 'Sign up').click
+      within ('#new_user_modal') do
+        fill_in 'Email', with: 'test@test.com'
+        fill_in 'Password', with: 'testtest'
+        fill_in 'Password confirmation', with: 'testtest'
+        click_button 'Sign up'
+      end
     end
 
-    fit 'should allow user to like a photo', js: true do
+    it 'should allow user to like a sighting', js: true do
       visit '/'
       click_link 'Like'
       expect(page).to have_content '1 like'
     end
 
+    it 'should not allow a user to like a sighting more than once', js: true do
+      visit '/'
+      click_link 'Like'
+      expect(page).not_to have_link 'Like'
+    end
+
+    it 'should let a user know when they have previously liked a sighting', js: true do
+      visit '/'
+      click_link 'Like'
+      visit '/'
+      expect(page).to have_content 'You liked this sighting'
+    end
   end
 
   context 'when not signed in' do
-    it 'should not allow the user to like a photo' do
+    it 'should not allow the user to like a sighting' do
       visit '/'
-
       expect(page).not_to have_link 'Like'
     end
   end
--- spec/features/shop_feature_spec.rb
+++ spec/features/shop_feature_spec.rb
@@ -17,6 +17,47 @@ describe 'Shop' do
       expect(page).to have_content 'Jumper'
       expect(page).to have_content 'Nice and warm'
     end
+
+    it 'the user should not see a link to the team info' do
+      visit '/shop'
+      expect(page).not_to have_link 'Team'
+    end
+
+    it 'the user should not see a link to the project info' do
+      visit '/shop'
+      expect(page).not_to have_link 'About'
+    end
+  end
+
+  context 'when on the shop page and logged in' do
+    before do
+      visit '/'
+      first(:link, 'Sign up').click
+      within ('#new_user_modal') do
+        fill_in 'Email', with: 'test@test.com'
+        fill_in 'Password', with: 'testtest'
+        fill_in 'Password confirmation', with: 'testtest'
+        click_button 'Sign up'
+      end
+      Item.create(name: "Jumper", description: "Nice and warm", price: 2)
+    end
+
+    it 'the user should see a buy now link' do
+      visit '/shop'
+      expect(page).to have_link 'Buy Now'
+    end
+
+    xit 'the user should be able to buy the item', js: true do
+      visit '/shop'
+      click_link 'Buy Now'
+      click_link 'Pay with Card'
+      fill_in 'Email', with: 'test@test.com'
+      fill_in 'Card number', with: '4242424242424242'
+      fill_in 'MM/YY', with: '06/16'
+      fill_in 'CVC', with: '444'
+      click_link 'Pay $2.00'
+      expect(page).to have_content 'Thanks, you paid'
+    end
   end
 
 
